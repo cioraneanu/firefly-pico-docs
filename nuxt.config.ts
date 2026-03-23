903459e3e988be70b965ce9d3d7cbc08d9bf322d
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2026-03-23 14:06:11 +0200

    - add context URL for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // @ts-ignore
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/firefly-pico-docs/',
+    },
     nitro: {
         prerender: {
             crawlLinks: true,
