64a7ec5b7f0e9849f14c44c94ec8571cb2f0991f
cioraneanu <cioraneanu@users.noreply.github.com>
2026-03-27 08:30:44 +0000

    deploy: f242c7220758e910aa062e396f2acea7dce49b90

---
+++ _nuxt/CXNN7qVC.js
@@ -0,0 +1,1 @@
+import{r}from"./DEPTPDBS.js";import t from"./CehF01pF.js";import{$ as a,a as e,p,s as c,o as n}from"./B9lWOpeZ.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=a();return(s,f)=>(n(),e(t,{color:"success",icon:c(o).ui.icons.tip},{default:p(()=>[r(s.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
