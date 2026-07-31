570283fc87dfaf2b3e352ed65b33b7895f2daa31
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2026-07-31 12:00:42 +0300

    - small fix to header link

---
+++ app/components/app/AppHeaderLeft.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+const site = useSiteConfig()
+</script>
+
+<template>
+  <NuxtLink
+    to="/introduction"
+    :aria-label="appConfig.header?.title || site.name"
+  >
+    <AppHeaderLogo class="h-6 w-auto shrink-0" />
+  </NuxtLink>
+</template>
